37b3157c9940b6ce87ac7641aa597e8bdcb2b1aa
secouba diedhiou <secouba.diedhiou@laplateforme.io>
2023-11-21 14:23:48 +0100

    op 4

--- Runtrack2/jours 7/jobs 4/index.php
+++ Runtrack2/jours 7/jobs 4/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello LaPlateforme</title>
+    <title>Calculatrice</title>
 </head>
 <body>
 
@@ -24,7 +24,7 @@ function calcule($a, $operation, $b) {
                 return "Division par zéro impossible";
             }
         case '%':
-        
+
             if ($b != 0) {
                 return $a % $b;
             } else {
@@ -35,17 +35,40 @@ function calcule($a, $operation, $b) {
     }
 }
 
-$resultatAddition = calcule(10, '+', 5);
-$resultatSoustraction = calcule(8, '-', 3);
-$resultatMultiplication = calcule(6, '*', 4);
-$resultatDivision = calcule(10, '/', 2);
-$resultatModulo = calcule(13, '%', 5);
+// Vérification si le formulaire a été soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupération des valeurs du formulaire
+    $nombre1 = isset($_POST['nombre1']) ? $_POST['nombre1'] : 0;
+    $operation = isset($_POST['operation']) ? $_POST['operation'] : '+';
+    $nombre2 = isset($_POST['nombre2']) ? $_POST['nombre2'] : 0;
 
-echo "Résultat de l'addition : $resultatAddition<br>";
-echo "Résultat de la soustraction : $resultatSoustraction<br>";
-echo "Résultat de la multiplication : $resultatMultiplication<br>";
-echo "Résultat de la division : $resultatDivision<br>";
-echo "Résultat du modulo : $resultatModulo<br>";
+    // Appel de la fonction calcule avec les valeurs du formulaire
+    $resultat = calcule($nombre1, $operation, $nombre2);
+
+    // Affichage du résultat
+    echo "Résultat de l'opération : $resultat";
+}
 ?>
+
+<!-- Formulaire pour saisir les valeurs -->
+<form method="post" action="">
+    <label for="nombre1">NombreX:</label>
+    <input type="text" name="nombre1" id="nombre1" value="<?php echo isset($nombre1) ? htmlspecialchars($nombre1) : ''; ?>" required>
+
+    <label for="operation">Opération :</label>
+    <select name="operation" id="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+        <option value="%">%</option>
+    </select>
+
+    <label for="nombre2">Nombrex:</label>
+    <input type="text" name="nombre2" id="nombre2" value="<?php echo isset($nombre2) ? htmlspecialchars($nombre2) : ''; ?>" required>
+
+    <button type="submit">Calculer</button>
+</form>
+
 </body>
 </html>
