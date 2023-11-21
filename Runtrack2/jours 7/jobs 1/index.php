73ecf1ede6900707cbe8b17a87426a333d8ed5d0
secouba diedhiou <secouba.diedhiou@laplateforme.io>
2023-11-21 10:11:12 +0100

    Create index.php

---
+++ Runtrack2/jours 7/jobs 1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello LaPlateforme</title>
+</head>
+<body>
+
+<?php
+function hello() {
+    echo "Hello LaPlateforme!";
+}
+
+// Appel de la fonction
+hello();
+?>
+
+</body>
+</html>
